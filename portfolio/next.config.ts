c391a3be5d93f12087eae350046cec1d3d54fec6
Muhammad Fawwaz Arshad Said <muhammad.fawwaz22@ui.ac.id>
2025-04-12 18:25:40 +0700

    feat: preparation build

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
+// Mendefinisikan tipe konfigurasi menggunakan NextConfig dari 'next'
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Peringatan: Ini memungkinkan build produksi selesai meskipun
+    // proyek Anda memiliki error ESLint.
+    ignoreDuringBuilds: true,
+  },
+  // Tambahkan opsi konfigurasi Next.js lainnya di sini jika diperlukan
 };
 
+// Mengekspor konfigurasi menggunakan export default
 export default nextConfig;
